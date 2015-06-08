eeacc036d31f65621cd9f19f780bd897c4837c61
Cal Heldenbrand <cal@fbsdata.com>
2015-06-08 10:44:23 -0500

    Deploy condition

--- app/controllers/deploy_controller.rb
+++ app/controllers/deploy_controller.rb
@@ -2,9 +2,11 @@ class DeployController < ApplicationController
   protect_from_forgery with: :null_session, only: [:webhook]
   def webhook
     # Super cheesy deployment system... for now
-    logger.info `git pull origin master`
-    logger.info `bundle install`
-    logger.info `touch #{Rails.root}/tmp/restart.txt`
+    if params[:ref] == "refs/heads/master" && params[:repository]["name"] == "testformvendor"
+      logger.info `git pull origin master`
+      logger.info `bundle install`
+      logger.info `touch #{Rails.root}/tmp/restart.txt`
+    end
     render json: {}
   end
 end
