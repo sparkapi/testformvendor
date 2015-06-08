b893efd661a69805af6330df6bdf7f6dc85b6fe2
Cal Heldenbrand <cal@fbsdata.com>
2015-06-08 10:33:10 -0500

    Adding deploy commands

--- app/controllers/deploy_controller.rb
+++ app/controllers/deploy_controller.rb
@@ -1,5 +1,9 @@
 class DeployController < ApplicationController
   protect_from_forgery with: :null_session, only: [:webhook]
   def webhook
+    # Super cheesy deployment system... for now
+    logger.info `git pull origin master`
+    logger.info `bundle install`
+    logger.info `touch #{Rails.root}/tmp/restart.txt`
   end
 end
