2b170812cdb80414553e50e35c6dc6d69cb5c196
Cal Heldenbrand <cal@fbsdata.com>
2015-06-08 10:01:05 -0500

    Remove CSRF token check for webhook

--- app/controllers/deploy_controller.rb
+++ app/controllers/deploy_controller.rb
@@ -1,5 +1,5 @@
 class DeployController < ApplicationController
-  #protect_from_forgery with: :null_session, only: [:properties]
+  protect_from_forgery with: :null_session, only: [:webhook]
   def webhook
   end
 end
