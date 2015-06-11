879d95a5d4729e14775666f5e5895595f4c25b44
Cal Heldenbrand <cal@fbsdata.com>
2015-06-11 11:15:53 -0500

    Another deploy test

--- app/controllers/deploy_controller.rb
+++ app/controllers/deploy_controller.rb
@@ -4,8 +4,11 @@ class DeployController < ApplicationController
     # Super cheesy deployment system... for now
     if params[:ref] == "refs/heads/master" && params[:repository]["name"] == "testformvendor"
       logger.info `git pull origin master`
-      logger.info `/usr/local/bin/bundle install`
+      logger.info "Running bundle install"
+      logger.info `bundle install`
+      logger.info "Running asset precompile"
       logger.info `RAILS_ENV=production /usr/local/bin/bundle exec rake assets:precompile`
+      logger.info "Restart app"
       logger.info `touch #{Rails.root}/tmp/restart.txt`
     end
     render json: {}
